bd3bdfb5bebb92ea240b633597af7873eca96b65
volkovVA <volkov.slava@hotmail.com>
2021-06-21 18:11:23 +0300

    feat: add webcam connection

--- 19-webcam-fun/scripts.js
+++ 19-webcam-fun/scripts.js
@@ -3,3 +3,29 @@ const canvas = document.querySelector('.photo');
 const ctx = canvas.getContext('2d');
 const strip = document.querySelector('.strip');
 const snap = document.querySelector('.snap');
+
+function getVideo() {
+  navigator.mediaDevices.getUserMedia({video: true, audio: false})
+    .then(localMediaStream => {
+      console.log(localMediaStream);
+      video.srcObject = localMediaStream;
+      video.play();
+    })
+    .catch(err => {
+      console.error('OH NOO!!!', err);
+    })
+}
+
+function paintToCanvas() {
+  const width = video.videoWidth;
+  const height = video.videoHeight;
+  canvas.width = width;
+  canvas.height = height;
+
+  return setInterval(() => {
+    ctx.drawImage(video, 0, 0, width, height);
+  }, 16);
+}
+
+
+getVideo();
